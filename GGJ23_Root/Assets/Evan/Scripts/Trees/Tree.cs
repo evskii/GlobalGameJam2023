aa9e9d2a4c39aed6d742b421be79b470908076b0
Evan <evskimo97@gmail.com>
2023-01-31 11:39:49 +0000

    Added more trees. Started on tree picker UI

--- GGJ23_Root/Assets/Evan/Scripts/Trees/Tree.cs
+++ GGJ23_Root/Assets/Evan/Scripts/Trees/Tree.cs
@@ -1,14 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public abstract class Tree : MonoBehaviour
+public abstract class Tree : MonoBehaviour, IDamageable
 {
+    [Header("Base Class Variables")]
+    public int maxHealth;
+    public int currentHealth;
+    public UnityEvent DeathEvent;
+
+    private void Awake() {
+        currentHealth = maxHealth;
+    }
+
     public virtual void Fire() {
         
     }
 
     public virtual void Think() {
         
+    }
+    public void TakeDamage(int amount) {
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    public void Die() {
+        DeathEvent.Invoke();
     }
 }
--- GGJ23_Root/Assets/Evan/Scripts/Trees/Tree_Hel.cs
+++ GGJ23_Root/Assets/Evan/Scripts/Trees/Tree_Hel.cs
@@ -9,8 +9,13 @@ public class Tree_Hel : Tree
 {
 	
 	public float fireRange;
+	public float damageRange;
 	private GameObject currentTarget;
 
+	public int damage;
+	public float damageRate;
+	private Coroutine damageCoroutine;
+	
 	public ParticleSystem fireParticle;
 
 	public bool showDebug = true;
@@ -25,16 +30,45 @@ public class Tree_Hel : Tree
 		
 		if (!currentTarget) {
 			fireParticle.Stop();
+			if (damageCoroutine != null) {
+				StopCoroutine(damageCoroutine);
+				damageCoroutine = null;
+			}
 			
 			var enemiesInRange = FindObjectsOfType<Transform>().Where(obj => obj.CompareTag("Enemy") && Vector3.Distance(transform.position, obj.position) <= fireRange).ToList();
 			if (enemiesInRange.Count > 0 && !currentTarget) {
 				currentTarget = enemiesInRange[0].gameObject;
 			}
 		} else {
+			if (Vector3.Distance(transform.position, currentTarget.transform.position) >= fireRange) {
+				currentTarget = null;
+				return;
+			}
+			
 			fireParticle.Play();
 			fireParticle.transform.LookAt(currentTarget.transform.position);
+			
+			
+			if (damageCoroutine == null) {
+				damageCoroutine = StartCoroutine(Damage());
+			}
 		}
 		
+	}
+	
+	private IEnumerator Damage() {
+		yield return new WaitForSeconds(damageRate);
+
+		if (currentTarget != null) {
+			var enemiesNearby = FindObjectsOfType<Transform>().Where(obj => obj.CompareTag("Enemy") 
+				                                                         && Vector3.Distance(currentTarget.transform.position, obj.position) <= damageRange).ToList();
+			foreach (var enemy in enemiesNearby) {
+				enemy.GetComponent<IDamageable>().TakeDamage(damage);
+			}
+			
+		}
+		
+		damageCoroutine = StartCoroutine(Damage());
 	}
 
 	
@@ -43,6 +77,11 @@ public class Tree_Hel : Tree
 		if (showDebug) {
 			Gizmos.color = Color.yellow;
 			Gizmos.DrawWireSphere(transform.position, fireRange);
+
+			if (currentTarget) {
+				Gizmos.color = Color.magenta;
+				Gizmos.DrawWireSphere(currentTarget.transform.position, damageRange);
+			}
 		}
 	}
 }
